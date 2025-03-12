fc88029e04b5daafdbbb8a1b76294b7d86479f30
jagadishck018 <jagadishck.018@gmail.com>
2025-03-12 11:50:26 +0530

    test index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ const movie1 = addMovie("The Matrix", "Wachowski", 1999, "Action");
 const movie2 = addMovie("Inception", "Christopher Nolan", 2010, "Action");
 const movie3 = addMovie("The Dark Knight", "Christopher Nolan", 2008, "Action");
 const movie4 = addMovie("Interstellar", "Christopher Nolan", 2014, "Sci-Fi");
+const movie5 = addMovie("The Dark Knight2", "Christopher Nolan", 2012, "Action");
 
 console.log("\n=== Movies Added Successfully ===\n");
 
@@ -23,12 +24,21 @@ rateMovie(movie1, 5);
 rateMovie(movie1, 4);
 rateMovie(movie2, 5);
 rateMovie(movie3, 4);
+rateMovie(movie3, 4);
+rateMovie(movie3, 4.5);
+rateMovie(movie3, 3.8);
 rateMovie(movie4, 5);
+rateMovie(movie5, 4.5)
+rateMovie(movie5, 4)
+rateMovie(movie5, 4.3)
+rateMovie(movie5, 4.1)
+rateMovie(movie5, 3.8)
+
 
 console.log("\n=== Ratings Added Successfully ===\n");
 
 // Get Average Rating
-console.log(`Average rating of "The Matrix": ${getAverageRating(movie1)}`);
+console.log(`Average rating of "The Matrix": ${getAverageRating(movie5)}`);
 
 // Get Top Rated Movies
 console.log("\n=== Top Rated Movies ===");
